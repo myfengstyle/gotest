1ec5ceb7f09668dc9ccf9112365d1554722d942a
fengstyle <myfengstyle@.github.com>
2019-12-12 18:16:12 +0800

    升级
    初始化更新12

---
+++ boot.go
@@ -0,0 +1,12 @@
+package boot
+
+import "fmt"
+
+func main() {
+	var a int = 21
+	var b int = 10
+	var c int
+
+	c = a + b
+	fmt.Printf("第一行 - c 的值为 %d\n", c)
+}
---
+++ main.go
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2019-12-12 17:45:26
+ * @LastEditTime: 2019-12-12 17:52:20
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \teste:\GitHub\gotest\gotest\main.go
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
